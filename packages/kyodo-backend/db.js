e0fb5eb0d2e39f1689b1738c865642ca73f61aa0
palevoo <kud.nikita@gmail.com>
2018-06-23 20:23:15 +0200

    added tip, domain, user, period schemas

--- packages/kyodo-backend/db.js
+++ packages/kyodo-backend/db.js
@@ -8,11 +8,12 @@ const taskSchema = new mongoose.Schema({
   specificationHash: String,
   finalized: Boolean,
   cancelled: Boolean,
+  dateCreated: Date,
   dueDate: Date,
   potId: Number,
-  domainId: Number,
-  id: Number,
-  skillId: Number
+  domainId: Number
+  // id: Number,
+  // skillId: Number
 });
 
 const colonySchema = new mongoose.Schema({
@@ -23,8 +24,43 @@ const colonySchema = new mongoose.Schema({
   tokenSymbol: String
 });
 
+const domainSchema = new mongoose.Schema({
+  domainId: Number,
+  domainTitle: String,
+  localSkillId: Number,
+  potId: Number
+});
+
+const tipSchema = new mongoose.Schema({
+  from: String,
+  to: String,
+  amount: Number,
+  taskId: Number,
+  domainId: Number,
+  dateCreated: Date,
+  periodId: Number
+});
+
+const userSchema = new mongoose.Schema({
+  address: String,
+  nickname: String,
+  tokenBalance: Number,
+  domains: Array,
+  tasks: Array,
+  dateCreated: Date
+});
+
+const periodSchema = new mongoose.Schema({
+  address: String,
+  periodId: Number,
+  tipBalance: Number
+});
+
 const Task = mongoose.model("Task", taskSchema);
-
 const Colony = mongoose.model("Colony", colonySchema);
+const Domain = mongoose.model("Domain", domainSchema);
+const Tip = mongoose.model("Tip", tipSchema);
+const User = mongoose.model("User", userSchema);
+const Period = mongoose.model("Period", periodSchema);
 
-module.exports = { Task, Colony };
+module.exports = { Task, Colony, Domain, Tip, User, Period };
