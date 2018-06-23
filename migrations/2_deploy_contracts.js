81556e2767ad9b39dfb671d55b83d86aa676a7eb
Igor Line <linesethenoiz@gmail.com>
2018-06-23 14:59:26 +0200

    Fixed deploy script for truffle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
 var KyodoDAO = artifacts.require('./KyodoDAO.sol');
 var DecentToken = artifacts.require('./DecentToken.sol');
 
-module.exports = async function(deployer, network, accounts) {
-  const token = await deployer.deploy(DecentToken);
-  await deployer.deploy(KyodoDAO, token.address);
+module.exports = (deployer, network, accounts) => {
+  deployer.deploy(DecentToken).then(() => {
+    return deployer.deploy(KyodoDAO, DecentToken.address);
+  });
 };
