a5b4fee582baf5261b89aa140d033c6722854e5d
palevoo <kud.nikita@gmail.com>
2018-06-23 13:35:36 +0200

    Added basic tip model without connection to the network or any balance checks

---
+++ packages/kyodo-backend/tip.js
@@ -0,0 +1,30 @@
+const { Tip } = require("./db.js");
+
+exports.sendTip = async (req, res) => {
+  // TODO: colonyClient integration
+
+  let tip = new Tip({
+    from: req.body.from,
+    to: req.body.to,
+    amount: req.body.amount,
+    taskId: req.body.taskId,
+    domainId: req.body.domainId,
+    dateCreated: new Date.now(),
+    periodId: req.body.periodId
+  });
+
+  tip.save((err, tip) => {
+    if (err) return console.error(err);
+  });
+  res.end(
+    `{"success" : Added ${tip} and ${hash} Successfully, "status" : 200}`
+  );
+};
+
+exports.getAllTips = async (req, res) => {
+  let tips = await Tip.find((err, tips) => {
+    if (err) return console.error(err);
+    console.log(tips);
+    res.send(`ALL AVAILABLE TASKS: ${tips}`);
+  });
+};
