c92105237275255bcade5912eb0b45821937e193
palevoo <kud.nikita@gmail.com>
2018-06-23 20:27:31 +0200

    updated getlatestblockmethod

--- packages/kyodo-backend/utils/getCurrentBlock.js
+++ packages/kyodo-backend/utils/getCurrentBlock.js
@@ -1,12 +1,10 @@
-//automated period cycles
 const Dagger = require("eth-dagger");
-
-// connect to Dagger ETH main network (network id: 1) over web socket
 const dagger = new Dagger("wss://mainnet.dagger.matic.network"); // dagger server
 
 exports.getCurrentBlock = async () => {
-  await dagger.once("latest:block.number", function(result) {
+  let latestBlock = await dagger.once("latest:block.number", result => {
     console.log("Current block number: ", result);
     return result;
   });
+  return latestBlock;
 };
