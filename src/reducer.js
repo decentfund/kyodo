ea442cca11c47886ce66348636bc300fe49eb68e
Igor Line <linesethenoiz@gmail.com>
2018-06-25 01:24:16 +0200

    Added load rate saga and logit

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { drizzleReducers } from 'drizzle';
+import { LOAD_RATE_SUCCESS } from './constants';
+
+const rate = (state = {}, action) => {
+  switch (action.type) {
+    case LOAD_RATE_SUCCESS:
+      return action.rate;
+    default:
+      return state;
+  }
+};
 
 const reducer = combineReducers({
   routing: routerReducer,
+  rate,
   ...drizzleReducers,
 });
 
--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,28 @@
-import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+import { takeEvery } from 'redux-saga';
+import { all, fork, call, put } from 'redux-saga/effects';
 import { drizzleSagas } from 'drizzle';
+import { LOAD_RATE_REQUEST, LOAD_RATE_SUCCESS } from './constants';
+
+function* loadRate() {
+  try {
+    const rateURI =
+      'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=EUR';
+    const eventLoad = yield call(axios.get, rateURI);
+    const rate = eventLoad.data['EUR'];
+
+    yield put({
+      type: LOAD_RATE_SUCCESS,
+      rate,
+    });
+  } catch (e) {}
+}
+
+function* watchLoadRate() {
+  yield* takeEvery(LOAD_RATE_REQUEST, loadRate);
+}
 
 export default function* root() {
   yield all(drizzleSagas.map(saga => fork(saga)));
+  yield fork(watchLoadRate);
 }
