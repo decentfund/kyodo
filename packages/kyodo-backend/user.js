cf31aebfb07747fa60e588310a8ee8071375acc8
palevoo <kud.nikita@gmail.com>
2018-06-24 18:00:35 +0200

    fixed User model import, added get all users and get user by id method , future methods outline and added response to add user method/endpoint

--- packages/kyodo-backend/user.js
+++ packages/kyodo-backend/user.js
@@ -1,4 +1,4 @@
-const User = require("./db.js");
+const { User } = require("./db.js");
 
 exports.addUser = async (req, res) => {
   //TODO: Colony network integration
@@ -6,21 +6,45 @@ exports.addUser = async (req, res) => {
 
   const user = new User({
     address: req.body.address,
-    nickname: req.body.nickname,
+    alias: req.body.alias,
     tokenBalance: req.body.tokenBalance,
     domains: req.body.domains,
     tasks: req.body.tasks,
-    dateCreated: new Date.now()
+    dateCreated: Date.now()
   });
 
   user.save();
-  res.send(`ALL GOOD! User ${user} saved successfully. Have a beer`);
+  res.status(200).send({
+    user: user,
+    message: "ALL GOOD! User saved successfully. Have a beer"
+  });
 };
 
-//TODO: getAllUsers ? ez
+exports.getAllUsers = async (req, res) => {
+  let users = await User.find((err, users) => {
+    if (err) return console.log(err);
+  });
+  res.status(200).send(users);
+  return users;
+};
 
-//TODO: findUserById or findUserByAddress ??? ORNOT
-
-exports.findUserByAddress = async (req, res) => {
+exports.getUserByAddress = async (req, res) => {
   await User.find({ address: req.body.address });
+};
+
+exports.getUserByAlias = async (req, res) => {
+  await User.find({ alias: req.body.alias });
+};
+
+exports.addDomainToUser = async (req, res) => {
+  // TODO:
+};
+
+exports.addTaskToUser = async (req, res) => {
+  // TODO:
+};
+
+exports.changeUserAlias = async (req, res) => {
+  // TODO:
+  // this.getUserByAlias(req.body.alias);
 };
