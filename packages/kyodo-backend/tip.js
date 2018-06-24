3a4813670fab09e68ce8e542538cc49bd84ed56d
palevoo <kud.nikita@gmail.com>
2018-06-24 17:58:37 +0200

    added integration with periods and balance checker helper, updated model write method

--- packages/kyodo-backend/tip.js
+++ packages/kyodo-backend/tip.js
@@ -1,18 +1,34 @@
 const { Tip } = require("./db.js");
+const {
+  changeUserBalance,
+  currentPeriod,
+  getUsebByAddressInPeriod
+} = require("./period.js");
 
 exports.sendTip = async (req, res) => {
   // TODO: colonyClient integration
   // transfer.send({ destinationAddress, amount }, options)
   // transferFrom.send({ sourceAddress, destinationAddress, amount }, options)
 
+  let tipAmount = req.body.amount;
+  let sender = getUsebByAddressInPeriod(req.body.from);
+  let balance = sender.balance;
+
+  if (!this.checkBalance(balance, tipAmount)) {
+    res.status(400).res.send("Inssuficient funds for the tip");
+  }
+
+  changeUserBalance(sender, tipAmount);
+
   let tip = new Tip({
-    from: req.body.from,
+    from: sender,
     to: req.body.to,
-    amount: req.body.amount,
+    amount: tipAmount,
     taskId: req.body.taskId,
     domainId: req.body.domainId,
-    dateCreated: new Date.now(),
-    periodId: req.body.periodId
+    potId: req.body.potId,
+    dateCreated: Date.now(),
+    periodId: currentPeriod
   });
 
   tip.save((err, tip) => {
@@ -29,4 +45,9 @@ exports.getAllTips = async (req, res) => {
     console.log(tips);
     res.send(`ALL AVAILABLE TASKS: ${tips}`);
   });
+};
+
+exports.checkBalance = (balance, amount) => {
+  if (amount <= balance && amount > 0) return true;
+  return false;
 };
