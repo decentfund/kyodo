c4f1063798df59379e69d3559e4708b1e760fbb6
Igor Line <linesethenoiz@gmail.com>
2018-06-24 19:28:38 +0200

    Added ProgressBar component. Closes #9

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 20px;
+  height: 7px;
+  background: #f5f905;
+  overflow: hidden;
+`;
+
+const StyledProgress = styled.div`
+  width: ${props => props.fraction * 100}%;
+  border-right: 1px solid #000000;
+  background: white;
+  height: 7px;
+`;
+
+const ProgressBar = ({ fraction }) => (
+  <StyledWrapper>
+    <StyledProgress fraction={fraction} />
+  </StyledWrapper>
+);
+
+ProgressBar.defaultProps = {
+  fraction: 0,
+};
+
+ProgressBar.propTypes = {
+  fraction: PropTypes.number,
+};
+
+export default ProgressBar;
