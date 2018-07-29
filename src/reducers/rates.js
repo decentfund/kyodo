24cb0bb36f38219d1b387f9f8e9c2e960ac153d1
Igor Line <linesethenoiz@gmail.com>
2018-07-29 18:19:52 +0200

    Store token data in redux store

--- src/reducers/rates.js
+++ src/reducers/rates.js
@@ -25,6 +25,24 @@ export default (
   switch (action.type) {
     case LOAD_RATE_SUCCESS:
       return { ...state, [action.currency]: action.rate };
+    case 'GET_CONTRACT_VAR':
+      if (action.name !== 'DecentToken') return state;
+      if (action.variable === 'symbol') {
+        return {
+          ...state,
+          [action.value]: { ...state[action.value] },
+          kyodoTokenSymbol: action.value,
+        };
+      } else if (action.variable === 'totalSupply') {
+        return {
+          ...state,
+          [state.kyodoTokenSymbol]: {
+            ...state[state.kyodoTokenSymbol],
+            totalSupply: action.value,
+          },
+        };
+      }
+      return { ...state, drizzle: action.drizzle };
     default:
       return state;
   }
