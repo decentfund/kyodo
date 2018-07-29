e2e44aae702b1fdb315103679d37262d05b40736
Igor Line <linesethenoiz@gmail.com>
2018-07-29 18:16:46 +0200

    Fixed currency formatting methods

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,2 +1,28 @@
-export const formatEth = value => `${value} ETH`;
-export const formatEur = value => `${value}€`;
+const ETH = 'ETH';
+const DAI = 'DAI';
+const USD = 'USD';
+const EUR = 'EUR';
+
+export function formatCurrency(value, currency, precision = 0) {
+  let decimalPoints = precision;
+  if (!precision) {
+    switch (currency) {
+      case USD:
+      case EUR:
+      case DAI:
+        decimalPoints = 2;
+        break;
+      case ETH:
+        decimalPoints = 4;
+        break;
+      default:
+        decimalPoints = 0;
+    }
+  }
+  return parseFloat(
+    Math.round(value * 10 ** decimalPoints) / 10 ** decimalPoints,
+  );
+}
+
+export const formatEth = value => `${formatCurrency(value, ETH)} ETH`;
+export const formatEur = value => `${formatCurrency(value, EUR)} €`;
