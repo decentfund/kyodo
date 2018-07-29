b1a6dccb14418b64c9e01f3f6a5c84acdfb5b2c4
Igor Line <linesethenoiz@gmail.com>
2018-07-29 17:58:54 +0200

    Calculate token price in FundStatistics

--- src/components/FundStatistics.js
+++ src/components/FundStatistics.js
@@ -3,18 +3,18 @@ import { ContractData } from 'drizzle-react-components';
 import { drizzleConnect } from 'drizzle-react';
 import StatisticsLabel from './StatisticsLabel';
 import { formatEth, formatEur } from '../helpers/format';
-import { getRate } from '../reducers';
+import { getRate, getTotalSupply, getContract } from '../reducers';
 
-const FundStatistics = ({ balance, balanceEur }) => (
+const FundStatistics = ({ balance, balanceEur, totalSupply }) => (
   <div>
     <StatisticsLabel label="SUP">
       <ContractData contract="DecentToken" method="totalSupply" /> tokens
     </StatisticsLabel>
     <StatisticsLabel label="CAP">
-      {formatEth(balance)} {formatEur(balanceEur)}
+      {formatEth(balance)} ~{formatEur(balanceEur)}
     </StatisticsLabel>
     <StatisticsLabel label="1 ◯">
-      {formatEth(0)} {formatEur(0)}
+      {formatEth(balance / totalSupply)} ~{formatEur(balanceEur / totalSupply)}
     </StatisticsLabel>
     <StatisticsLabel label="FUND">
       <ContractData contract="KyodoDAO" method="getMembersCount" /> members
@@ -27,6 +27,7 @@ const mapStateToProps = state => {
   return {
     balance,
     balanceEur: balance * getRate(state, 'ETH', 'EUR'),
+    totalSupply: getTotalSupply(getContract('DecentToken')(state)),
   };
 };
 
