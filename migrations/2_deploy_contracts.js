d6d37b7d9ccfd5f7941ba81be7948efdda60c47f
Igor Line <linesethenoiz@gmail.com>
2018-07-29 17:44:49 +0200

    Mint tokens to ganache addresses

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,14 @@ var DecentToken = artifacts.require('./DecentToken.sol');
 module.exports = (deployer, network, accounts) => {
   deployer
     .deploy(DecentToken)
-    .then(() => deployer.deploy(KyodoDAO, DecentToken.address))
     .then(() => {
-      DecentToken.at(DecentToken.address).transferOwnership(KyodoDAO.address);
+      DecentToken.at(DecentToken.address).mint(accounts[0], 752);
+      DecentToken.at(DecentToken.address).mint(accounts[1], 7260 - 752);
     })
+    .then(() => deployer.deploy(KyodoDAO, DecentToken.address))
+    // .then(() => {
+    // DecentToken.at(DecentToken.address).transferOwnership(KyodoDAO.address);
+    // })
     .then(() => {
       KyodoDAO.at(KyodoDAO.address).startNewPeriod();
       KyodoDAO.at(KyodoDAO.address).setPeriodDaysLength(14);
