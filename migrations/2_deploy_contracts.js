cc2a061d5887925308fdc74a8d22329fb52f8825
Igor Line <linesethenoiz@gmail.com>
2018-08-21 09:01:22 +0200

    Added colony members in migrations, fixed deploy_parameters structure

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,13 @@
-var KyodoDAO = artifacts.require('./KyodoDAO.sol');
 var DecentToken = artifacts.require('./DecentToken.sol');
-var deployParameters = require('./deploy_parameters');
+var deployParameters = require('./deploy_parameters.json');
 var tokenInstance;
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
     tokenInstance = await deployer.deploy(DecentToken);
-    await deployParameters.accounts.map(account => {
-      for (var key in account) {
-        if (account.hasOwnProperty(key)) {
-          tokenInstance.mint(key, account[key]);
-        }
-      }
-      return true;
+    const { accounts } = deployParameters;
+    Object.keys(accounts).forEach(address => {
+      tokenInstance.mint(address, accounts[address]);
     });
   });
 };
