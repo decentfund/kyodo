14ebf31ac2c84b856a7b90a517897e340777a917
Igor Line <linesethenoiz@gmail.com>
2018-08-25 18:56:14 +0200

    Minting reserve tokens #68

--- packages/kyodo/migrations/3_deploy_kyodo.js
+++ packages/kyodo/migrations/3_deploy_kyodo.js
@@ -1,3 +1,4 @@
+var sum = require('lodash/sum');
 var KyodoDAO = artifacts.require('./KyodoDAO.sol');
 var DecentToken = artifacts.require('./DecentToken.sol');
 var deployParameters = require('./deploy_parameters.json');
@@ -8,6 +9,9 @@ module.exports = (deployer, network, accounts) => {
     const { accounts } = deployParameters;
     const addresses = Object.keys(accounts);
     await KyodoDAO.at(KyodoDAO.address).addManyToWhitelist(addresses);
+    const totalMembersTokens = sum(Object.values(accounts));
+    const reserve = totalMembersTokens * 0.5;
+    await DecentToken.at(DecentToken.address).mint(KyodoDAO.address, reserve);
     await KyodoDAO.at(KyodoDAO.address).startNewPeriod();
     return KyodoDAO.at(KyodoDAO.address).setPeriodDaysLength(14);
   });
