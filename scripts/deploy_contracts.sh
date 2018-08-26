3b9909ab5fa14c99ecb9c4f8a9511c5544a2619c
Igor Line <linesethenoiz@gmail.com>
2018-08-26 18:34:52 +0200

    Updated script to deploy contracts

--- scripts/deploy_contracts.sh
+++ scripts/deploy_contracts.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Move to colonyNetwork directory
+cd packages/colonyNetwork
+
+# Compile and deploy colonyNetwork contracts
+./node_modules/.bin/truffle migrate --compile-all --reset
+
 # Move to Kyodo directory
-cd packages/kyodo
+cd ../kyodo
 
 # Compile and deploy Kyodo contracts
 ./node_modules/.bin/truffle migrate --compile-all --reset
