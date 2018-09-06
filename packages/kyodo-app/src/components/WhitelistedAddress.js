9a1675e79f8ee844f73d433fd5bbfe16446de191
Igor Line <linesethenoiz@gmail.com>
2018-09-06 19:35:44 +0200

    Displaying riot id from blockchain in member whitelisted address. Closes

--- packages/kyodo-app/src/components/WhitelistedAddress.js
+++ packages/kyodo-app/src/components/WhitelistedAddress.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { drizzleConnect } from 'drizzle-react';
 import styled from 'styled-components';
 import FormattedAddress from './FormattedAddress';
+import { getContract } from '../reducers';
 
 const StyledContainer = styled.div`
   display: flex;
@@ -25,14 +28,48 @@ const StyledAlias = StyledDiv.extend`
   overflow: hidden;
   white-space: nowrap;
 
-  color: ${props => (props.placeholder ? GRAY : BLACK)};
+  color: ${props => (props.isPlaceholder ? GRAY : BLACK)};
 `;
 
-const WhitelistedAddress = ({ alias, value }) => (
-  <StyledContainer>
-    <StyledAlias placeholder={!alias}>{alias || 'member known as'}</StyledAlias>
-    <FormattedAddress>{value}</FormattedAddress>
-  </StyledContainer>
-);
+class WhitelistedAddress extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.contracts = context.drizzle.contracts;
+  }
+  componentDidMount() {
+    const contract = this.contracts.KyodoDAO;
 
-export default WhitelistedAddress;
+    // let drizzle know we want to watch the `myString` method
+    const dataKey = contract.methods.getAlias.cacheCall(this.props.value);
+
+    // save the `dataKey` to local component state for later reference
+    this.setState({ dataKey });
+  }
+  render() {
+    let alias =
+      this.state &&
+      this.state.dataKey &&
+      this.props.KyodoDAO.getAlias[this.state.dataKey];
+    const { value } = this.props;
+
+    return (
+      <StyledContainer>
+        <StyledAlias isPlaceholder={!(alias && alias.value)}>
+          {(alias && alias.value) || 'member known as'}
+        </StyledAlias>
+        <FormattedAddress>{value}</FormattedAddress>
+      </StyledContainer>
+    );
+  }
+}
+
+WhitelistedAddress.contextTypes = {
+  drizzle: PropTypes.object,
+};
+
+const mapStateToProps = (state, { account }) => ({
+  drizzleStatus: state.drizzleStatus,
+  KyodoDAO: getContract('KyodoDAO')(state),
+});
+
+export default drizzleConnect(WhitelistedAddress, mapStateToProps);
