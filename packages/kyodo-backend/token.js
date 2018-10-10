cb4ae9d3dbc396d5e78b432d38687cf7da2b4f61
Igor Line <linesethenoiz@gmail.com>
2018-10-10 18:22:35 +0200

    fixed backend token connection

--- packages/kyodo-backend/token.js
+++ packages/kyodo-backend/token.js
@@ -1,5 +1,7 @@
 const Web3 = require('web3');
-const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
+const provider = new Web3.providers.WebsocketProvider(
+  process.env.WS_PROVIDER || 'ws://localhost:8545',
+);
 const TruffleContract = require('truffle-contract');
 const Token = require('@kyodo/contracts/build/contracts/Token.json');
 
@@ -15,6 +17,7 @@ const initializeToken = async () => {
 };
 
 const getBalance = async (address, blockNumber) => {
+  if (!token) token = await initializeToken();
   const balance = await token.balanceOf(address, blockNumber);
   const decimals = await token.decimals();
   return balance / Math.pow(10, decimals);
