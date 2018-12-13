f16f91f5fcd9b7b00d8be0f3fa770597f1d2bcac
Igor Line <linesethenoiz@gmail.com>
2018-12-14 00:22:54 +0100

    Fix ordering for tips
    Closes #237

--- packages/kyodo-app/src/rootSaga.js
+++ packages/kyodo-app/src/rootSaga.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import moment from 'moment';
+import orderBy from 'lodash/orderBy';
 import {
   all,
   fork,
@@ -24,7 +25,8 @@ import {
 import { BASE_CURRENCY } from './constants';
 import * as fromActions from './actions';
 
-const BACKEND_URI = process.env.REACT_APP_BACKEND_URI || 'http://kyodo.decent.fund:3666';
+const BACKEND_URI =
+  process.env.REACT_APP_BACKEND_URI || 'http://kyodo.decent.fund:3666';
 
 function* loadRate({ currency }) {
   try {
@@ -122,16 +124,21 @@ function* loadPeriodTasks() {
     const apiURI = `${BACKEND_URI}/tips`;
 
     const { data } = yield call(axios.get, apiURI);
-    const tasks = data.map(t => ({
-      title: t.task.taskTitle,
-      domain: t.domain.domainTitle,
-      from: t.from.alias || t.from.address,
-      fromAddress: t.from.address,
-      to: t.to.alias || t.to.address,
-      toAddress: t.to.address,
-      id: t._id,
-      amount: t.amount,
-    }));
+    const tasks = orderBy(
+      data.map(t => ({
+        title: t.task.taskTitle,
+        domain: t.domain.domainTitle,
+        from: t.from.alias || t.from.address,
+        fromAddress: t.from.address,
+        to: t.to.alias || t.to.address,
+        toAddress: t.to.address,
+        id: t._id,
+        amount: t.amount,
+        dateCreated: t.dateCreated,
+      })),
+      ['dateCreated'],
+      ['desc'],
+    );
 
     yield put({
       type: LOAD_PERIOD_TASKS_SUCCESS,
