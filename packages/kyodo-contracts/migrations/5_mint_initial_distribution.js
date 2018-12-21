c9faeae674790fe215eb1c8d9866f89798f5a045
Igor Line <linesethenoiz@gmail.com>
2018-12-21 08:48:37 +0100

    Fix token decimal issues in initial migration

--- packages/kyodo-contracts/migrations/5_mint_initial_distribution.js
+++ packages/kyodo-contracts/migrations/5_mint_initial_distribution.js
@@ -12,17 +12,24 @@ module.exports = deployer => {
     const { accounts: distAccounts } = deployParameters;
     // Minting initial distribution
     Object.keys(distAccounts).forEach(async address => {
-      const amount = distAccounts[address] * Math.pow(10, 18);
-      await tokenInstance.mint(amount);
-      await tokenInstance.transfer(address, amount);
+      const specifiedAmount = distAccounts[address];
+      if (specifiedAmount > 0) {
+        // FIXME: this works for tokens with 18 decimals only
+        const convertedAmount = web3.utils.toWei(
+          specifiedAmount.toString(),
+          'ether',
+        );
+        await tokenInstance.mint(convertedAmount);
+        await tokenInstance.transfer(address, convertedAmount);
+      }
     });
     // Minting reserve tokens
     let totalToMint = Object.values(distAccounts).reduce(
       (acc, value) => acc + value,
       0,
     );
-    const reserve = totalToMint * 0.5 * Math.pow(10, 18);
-    await tokenInstance.mint(reserve);
+    const reserve = totalToMint * 0.5;
+    await tokenInstance.mint(web3.utils.toWei(reserve.toString(), 'ether'));
     // TODO: Transfer to multisig
     await tokenInstance.transfer(kyodoInstance.address, reserve);
     // Pass ownership to KyodoDAO
