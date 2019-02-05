eb3c02a7f3ad7c26e237da7b42a034ddcd40f4b1
Igor Line <linesethenoiz@gmail.com>
2019-02-05 17:32:02 +0100

    Remove connect-redux-router

--- packages/kyodo-app/src/store.js
+++ packages/kyodo-app/src/store.js
@@ -1,27 +1,21 @@
-import { createBrowserHistory } from 'history';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'connected-react-router';
-import createRootReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
 import rootSaga from './rootSaga';
 import createSagaMiddleware from 'redux-saga';
 import { Drizzle, generateContractsInitialState } from 'drizzle';
 import drizzleOptions from './drizzleOptions';
 
 // Redux DevTools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const sagaMiddleware = createSagaMiddleware();
 
 const initialState = {
   contracts: generateContractsInitialState(drizzleOptions),
 };
 
-export const history = createBrowserHistory();
-
 const store = createStore(
-  createRootReducer(history),
+  rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware)),
+  applyMiddleware(sagaMiddleware),
 );
 
 sagaMiddleware.run(rootSaga);
