ab70f4339a534acd3802fc153a461d7a7be4770f
Igor Line <linesethenoiz@gmail.com>
2019-02-11 19:07:14 +0100

    Restore redux dev tools middleware

--- packages/kyodo-app/src/store.js
+++ packages/kyodo-app/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import rootSaga from './rootSaga';
 import createSagaMiddleware from 'redux-saga';
@@ -6,6 +6,8 @@ import { Drizzle, generateContractsInitialState } from 'drizzle';
 import drizzleOptions from './drizzleOptions';
 
 // Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware();
 
 const initialState = {
@@ -15,7 +17,7 @@ const initialState = {
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(sagaMiddleware),
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);
