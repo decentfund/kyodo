5e6fc5a22ef9dd6017813806b133eba1ccd39f32
Igor Line <linesethenoiz@gmail.com>
2019-03-07 17:53:43 +0100

    Return actual big number from convertAmount token util in shared package

--- packages/kyodo-shared/token.js
+++ packages/kyodo-shared/token.js
@@ -8,5 +8,5 @@ const { BN } = require("bn.js");
  */
 export const convertAmount = (amount, decimals) => {
   const WAD = new BN(10).pow(new BN(parseInt(decimals)));
-  return WAD.muln(parseFloat(amount)).toString();
+  return WAD.muln(parseFloat(amount));
 };
